2fd16c8f96b2991f57d4127b8f8ebf490eba5709
Augustin Godiscal <hello@auggod.tech>
2022-07-07 13:00:38 +0200

    FIX: Should really not extend default redis conf object here

--- src/config/cache.js
+++ src/config/cache.js
@@ -18,7 +18,7 @@ const cleanKey = (key) => {
 }
 
 const memoryCache = new NodeCache({ stdTTL: 100, checkperiod: 120 })
-const redis = new Redis(Object.assign(redisConfig, {
+const redis = new Redis(Object.assign({}, redisConfig, {
   keyPrefix: Date.now().toString(),
   lazyConnect: process.env.NODE_ENV !== 'production' // avoid loading redis in development, especially if connection is remote
 }))
